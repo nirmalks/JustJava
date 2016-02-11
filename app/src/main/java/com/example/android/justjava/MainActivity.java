b3709851c003002f477a2ca4393befae8ed49e44
nirmalks <kagosekar@gmail.com>
2016-02-11 23:03:24 +0530

    Removed displayPrice method

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -38,15 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
+    
     public void increment(View view){
         quantity += 1;
         displayQuantity(quantity);
